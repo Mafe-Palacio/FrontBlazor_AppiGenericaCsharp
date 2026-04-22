201d8fbc426d064f77d51faa199f139afc51276a
Mafe Palacio <mafe.palacio53@gmail.com>
2026-04-22 04:05:35 -0500

    Se agrega el componente maestro detalle de las tablas grupo investigacion y semillero

--- obj/Debug/net9.0/FrontBlazor_AppiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontBlazor_AppiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8480ed38c3580e420386dda1951299a42a84177b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf2d33953654c8bdddca83b65fd09af03b45be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
