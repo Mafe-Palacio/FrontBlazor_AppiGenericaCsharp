6cd0863d8cf1d5a01cb02615238d8e33e7d4f79b
Mafe Palacio <mafe.palacio53@gmail.com>
2026-05-04 07:56:19 -0500

    Se mejoró lo visual al componente maestro detalle y se agregaron mas class para la sub tabla en el css

--- obj/Debug/net9.0/FrontBlazor_AppiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontBlazor_AppiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf2d33953654c8bdddca83b65fd09af03b45be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201d8fbc426d064f77d51faa199f139afc51276a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontBlazor_AppiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
